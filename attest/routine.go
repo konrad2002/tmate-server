d529a9ed34f0d3344f825bed3b2227ae05a10c98
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-03-23 18:06:51 +0100

    run attest routine at 7pm

--- attest/routine.go
+++ attest/routine.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-var notificationHour = 18
+var notificationHour = 19
 
 func StartAttestRoutine(attestService service.AttestService, fieldService service.FieldService, configService service.ConfigService, emailService service.EmailService) {
 	go func() {
